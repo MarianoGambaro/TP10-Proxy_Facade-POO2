b57d13fb2d0f2cdb28b61c1e24a2075da5463c9f
MarianoGambaro <martinezgambaromariano@gmail.com>
2026-06-10 00:31:19 -0600

    punto 1

--- src/main/java/pt1/Main.java
+++ src/main/java/pt1/Main.java
@@ -11,3 +11,16 @@ public class Main {
         }
     }
 }
+
+/* Sujeto : interfaz Set, define el comportamiento comun
+
+ * Sujeto Real : HashSet, la colección que guarda los telefonos
+   traidos de la BD. Es el objeto que queremos encapsular
+
+ * Proxy : TelefonoSetProxy, la clase que se "hace pasar por los telefonos",
+   intercepta las llamadas a la interfaz Set para retrasar la consulta SQL
+
+ * Cliente: la clase Persona, interactua con el sujeto (Set)
+   sin notar la dif enrte el sujeto real y el proxy
+
+ */
